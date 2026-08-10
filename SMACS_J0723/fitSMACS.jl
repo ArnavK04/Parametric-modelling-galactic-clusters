58fc4870e613ef6d406b4b76543a593a2bbacff7
Arnav Kumar <177550453+ArnavK04@users.noreply.github.com>
2026-08-10 19:06:23 +0000

    corrected fit file

--- SMACS_J0723/fitSMACS.jl
+++ SMACS_J0723/fitSMACS.jl
@@ -1,6 +1,6 @@
 using LensFactory
 
 # Parse the YAML file
-parameters = LensModel.read_input("ARES_input.yaml")
+parameters = LensModel.read_input("SMACS0723_input.yaml")
 
 LensModel.fit_model(parameters)
