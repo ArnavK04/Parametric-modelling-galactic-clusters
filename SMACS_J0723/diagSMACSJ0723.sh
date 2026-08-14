5611d59426687f93f75bf8838161ed192ee3d2aa
ArnavK04 <177550453+ArnavK04@users.noreply.github.com>
2026-08-14 15:20:10 +0530

    syncing

--- SMACS_J0723/diagSMACSJ0723.sh
+++ SMACS_J0723/diagSMACSJ0723.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #PBS -q workq
-#PBS -N pm_SMACS0723test0
-#PBS -l nodes=comp1:ppn=16
+#PBS -N diagpmSMACS0723test0
+#PBS -l nodes=comp1:ppn=1
 #PBS -V
 
-LOGNAME="SMACSJ0723_test0"
+LOGNAME="SMACSJ0723_test0_analyse"
 
 exec > "/home/arnav/Parametric/logfiles/logfiles/${LOGNAME}.log" 2> "/home/arnav/Parametric/logfiles/errfiles/${LOGNAME}.err"
 
 cd /home/arnav/Parametric/Parametric-modelling-galactic-clusters/SMACS_J0723
 
-~/.juliaup/bin/julia --project=/home/arnav/UpdatedLensFactoryEnv -t 16 fitSMACS.jl
+~/.juliaup/bin/julia --project=/home/arnav/UpdatedLensFactoryEnv analyseSMACS.jl
